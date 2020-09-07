a33c72f01a9a353f0e9af6c63cfa338df902cce9
Dhruv <guptadhruv.2001@gmail.com>
2020-09-07 09:57:45 +0530

    Fixed mwf

--- home/app.js
+++ home/app.js
@@ -18,6 +18,18 @@ function setFromStorage(){
   let todayClasses = [];
   if(items) {
     todayClasses = items.filter(item=>item.day==days[day]);
+    items.forEach(item=>{
+      if(item.day=='1'){
+        if(day==1||day==3||day==5){
+          todayClasses.push(item);
+        }
+      }
+      else if(item.day=='2'){
+        if(day==2||day==4||day==6){
+          todayClasses.push(item);
+        }
+      }
+    })
   }
   let flag = false;
 
@@ -25,7 +37,7 @@ function setFromStorage(){
   todayClasses.forEach(subject=>{
     let classTime = (parseInt(subject.time[5])+7)%12;
     let meetLink = `https://meet.google.com/${subject.link}`;
-    if(classTime==hour){
+    if((classTime==hour&&minutes<45)||(classTime-1==hour&&minutes>45)){
       let node = document.createElement('div');
 
       box.innerHTML = `<h3 class='boxText'>You have a class.</h3>`
